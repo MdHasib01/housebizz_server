2c15092fdf769e6cfbbfd202585df0389220b7be
MdHasib01 <md.hasibuzzaman001@gmail.com>
2024-12-28 02:54:40 +0600

    fix path issue

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,29 +1,10 @@
 import multer from "multer";
 import path from "path";
-import { promises as fs } from "fs";
 
-const tempPath = path.join(process.cwd(), "public", "temp"); // Absolute path
-
-// Ensure the directory exists
-const ensureDirectoryExists = async (dirPath) => {
-  try {
-    await fs.mkdir(dirPath, { recursive: true });
-  } catch (error) {
-    if (error.code !== "EEXIST") {
-      throw error;
-    }
-  }
-};
-
-// Setup multer storage
 const storage = multer.diskStorage({
-  destination: async function (req, file, cb) {
-    try {
-      await ensureDirectoryExists(tempPath); // Ensure the directory exists
-      cb(null, tempPath);
-    } catch (error) {
-      cb(error);
-    }
+  destination: function (req, file, cb) {
+    const tempDir = path.join(process.cwd(), "public", "temp");
+    cb(null, tempDir);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
